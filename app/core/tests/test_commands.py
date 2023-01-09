adf11b5fd80dda1df0e1123348d405ea75ebc2a4
Josh Bright <josh.bright@gmail.com>
2023-01-09 09:48:08 -0800

    Fixed test

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -16,7 +16,7 @@ class CommandTests(SimpleTestCase):
         """Test waiting for database if database ready."""
         patched_check.return_value = True
         call_command('wait_for_db')
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
     def test_wait_for_db_delay(self, patched_check):
         """Test waiting for db delay"""
@@ -24,4 +24,4 @@ class CommandTests(SimpleTestCase):
             [OperationalError] * 3 + [True]
         call_command('wait_for_db')
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(database=['default'])
+        patched_check.assert_called_with(databases=['default'])
