1ade7e89b097e229b2d59ad1d8135d4143e66e4d
Josh Bright <josh.bright@gmail.com>
2023-01-09 16:59:26 -0800

    Updated serializer to handle updates

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -18,7 +18,25 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
     def create(self, validated_data):
+        """Create a new user from validated data and return it."""
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update and return user."""
+
+        # pull the password from the update data
+        password = validated_data.pop('password', None)
+
+        # update the user instance with the rest of the fields
+        user = super().update(instance, validated_data)
+
+        # we need to treat passwords in a special way, so that's why
+        # we pulled the data out of the initial update
+        if password:
+            # we were passed a new password, so update it
+            user.set_password(password)
+            user.save()
+        return user
 
 
 class AuthTokenSerializer(serializers.Serializer):
