46c71d015122cec7383db89a31868166619e140b
Josh Bright <josh.bright@gmail.com>
2023-01-12 19:43:32 -0800

    Updated system to be able to handle media files

--- app/recipe_app/urls.py
+++ app/recipe_app/urls.py
@@ -19,6 +19,9 @@ from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -35,3 +38,9 @@ urlpatterns = [
     path('api/users/', include('user.urls')),
     path('api/recipe/', include('recipe.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
